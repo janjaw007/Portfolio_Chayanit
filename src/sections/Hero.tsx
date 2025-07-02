6edc177082df838ada4991c3f54a3324923fb547
janjaw007 <chayanit.kitm@gmail.com>
2025-07-02 23:45:32 +0700

    hero-section tablet size finish

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from "@/assets/icons/arrow-down.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48">
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -20,14 +20,16 @@ export const HeroSection = () => {
           </div>
         </div>
 
-        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
-          Building Exceptional User Experiences
-        </h1>
-        <p className="mt-4 text-center text-white/60">
-          I specialize in transforming designs into functional, high-performing
-          web applications. Let's discuss your next project.
-        </p>
-        <div className="flex flex-col items-center mt-8 gap-4">
+        <div className="max-w-lg mx-auto">
+          <h1 className="md:text-5xl font-serif text-3xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in transforming designs into functional,
+            high-performing web applications. Let's discuss your next project.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button
             type="button"
             className=" inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl"
